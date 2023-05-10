9c8eb089d56c4e2363af0523edb1eeb537c3412f
02-t <alexcelcinschi@gmail.com>
2023-05-10 12:37:43 +0300

    added interface for better visualization

--- chart_generator.py
+++ chart_generator.py
@@ -5,6 +5,8 @@ import matplotlib.ticker as ticker
 import mplcursors
 import locale
 
+from threading import Thread
+
 locale.setlocale(locale.LC_ALL, '')
 
 df = pd.read_csv('data.csv')
@@ -13,6 +15,7 @@ df = df.dropna()
 df = df[df['production year'].apply(lambda x: str(x).isdigit())]
 df = df[df['price'].apply(lambda x: str(x).isdigit())]
 df = df[df['km driven'].apply(lambda x: str(x).isdigit())]
+df = df[df['fuel'] != '-']
 
 df['production year'] = df['production year'].astype(int)
 df['price'] = df['price'].astype(float)
@@ -30,23 +33,25 @@ def get_title(string):
     return string
 
 
-def run_chart(x, y):
+def run_chart(x, y, format, color):
     global df
+
+    title_x = get_title(x)
+    title_y = get_title(y)
 
     df = df.sort_values('km driven')
 
     sns.set_style('whitegrid')
-    sns.lineplot(data=df, x=x, y=y, color='#610076')
+    sns.lineplot(data=df, x=x, y=y, color=color)
 
-    title_x = get_title(x)
-    title_y = get_title(y)
 
     plt.title(title_y + ' vs ' + title_x)
     plt.ylabel(title_y)
     plt.xlabel(title_x)
 
     gca = plt.gca()
-    gca.xaxis.set_major_formatter(ticker.FuncFormatter(format_tick))
+    if format:
+        gca.xaxis.set_major_formatter(ticker.FuncFormatter(format_tick))
     gca.yaxis.set_major_formatter(ticker.FuncFormatter(format_tick))
 
     cursor = mplcursors.cursor(hover=True)
@@ -55,11 +60,16 @@ def run_chart(x, y):
     @cursor.connect("add")
     def on_add(sel):
         x0 = sel.target[0]
-        nearest_index = df[x].sub(x0).abs().idxmin()
-        nearest_row = df.loc[nearest_index]
-        y_val = locale.format_string("%d", nearest_row[y], grouping=True)
-        x_val = locale.format_string("%d", nearest_row[x], grouping=True)
-        sel.annotation.set_text(f'{title_y}: {y_val}\n{title_x}: {x_val}')
+        try:
+            nearest_index = df[x].sub(x0).abs().idxmin()
+            nearest_row = df.loc[nearest_index]
+            y_val = locale.format_string("%d", nearest_row[y], grouping=True)
+            x_val = str(nearest_row[x])
+            if format:
+                x_val = locale.format_string("%d", nearest_row[x], grouping=True)
 
-    plt.gcf().canvas.set_window_title(f'{title_y} vs {title_x}')
+            sel.annotation.set_text(f'{title_y}: {y_val}\n{title_x}: {x_val}')
+        except:
+            pass
+
     plt.show()
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-import chart_generator
+import tkinter as tk
+from threading import Thread
 
-chart_generator.run_chart('km driven', 'price')
+import chart_generator as cg
+
+
+def run_chart(x, y, f, c):
+    t1 = Thread(target=cg.run_chart, args=(x, y, f, c))
+    t1.start()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    fuel_button = tk.Button(root,
+                            text="Run Fuel Chart",
+                            command=lambda: run_chart('fuel', 'price', False, 'blue'))
+    fuel_button.pack()
+
+    production_year_button = tk.Button(root,
+                                       text="Run Production Year Chart",
+                                       command=lambda: run_chart('production year', 'price', False, 'red'))
+    production_year_button.pack()
+
+    km_driven_button = tk.Button(root,
+                                 text="Run Km Driven Chart",
+                                 command=lambda: run_chart('km driven', 'price', True, '#610076'))
+    km_driven_button.pack()
+
+    root.mainloop()
